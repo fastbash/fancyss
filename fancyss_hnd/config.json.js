ec89f9f253a73ab9f21a2878fbafd22fba7c7adb
fastbash <yovfal@gmail.com>
2019-12-24 00:08:56 +0800

    change haproxy backend roundrobin to source;

--- fancyss_hnd/config.json.js
+++ fancyss_hnd/config.json.js
@@ -1,10 +1,10 @@
 {
-"build_date":"2019-11-01_03:11:47",
+"build_date":"2019-12-23_23:59:02",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"0d4807f83f2ae181250d88f35184c2a0",
+"md5":"3f7aeac23bc0013325198a06efe4aa56",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/fastbash/fancyss/master/fancyss_hnd/shadowsocks.tar.gz",
 "title":"科学上网",
-"version":"1.7.5"
+"version":"1.7.6"
 }
