b98725c33b0cabc55ec68b602589db524d676d8a
fastbash <yovfal@gmail.com>
2020-06-18 22:14:08 +0800

    fix update location;

--- fancyss_hnd/planesocks/scripts/ss_update.sh
+++ fancyss_hnd/planesocks/scripts/ss_update.sh
@@ -33,8 +33,8 @@ update_ss(){
 			cd /tmp
 			md5_web1=`curl -s "$main_url"/version | sed -n 2p`
 			echo_date 开启下载进程，从主服务器上下载更新包...
-			wget --no-check-certificate --timeout=5 "$main_url"/shadowsocks.tar.gz
-			md5sum_gz=`md5sum /tmp/shadowsocks.tar.gz | sed 's/ /\n/g'| sed -n 1p`
+			wget --no-check-certificate --timeout=5 "$main_url"/planesocks.tar.gz
+			md5sum_gz=`md5sum /tmp/planesocks.tar.gz | sed 's/ /\n/g'| sed -n 1p`
 			if [ "$md5sum_gz" != "$md5_web1" ]; then
 				echo_date 更新包md5校验不一致！估计是下载的时候出了什么状况，请等待一会儿再试...
 				rm -rf /tmp/planesocks* >/dev/null 2>&1
@@ -65,7 +65,7 @@ update_ss(){
 update_ss2(){
 	echo_date "目前还没有任何备用服务器！请尝试使用离线安装功能！"
 	echo_date "历史版本下载地址：https://github.com/fastbash/fancyss_history_package/tree/master/fancyss_hnd"
-	echo_date "下载后请将下载包名字改为：shadowsocks.tar.gz，再使用软件中心离线安装功能进行安装！"
+	echo_date "下载后再使用软件中心离线安装功能进行安装！"
 	sleep 1
 	echo XU6J03M6
 	exit
