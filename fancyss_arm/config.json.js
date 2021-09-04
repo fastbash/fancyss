ee489dfb38c08fb15389ccf6465c232997381268
fastbash <fastbash@hotmail.com>
2021-09-04 18:15:43 +0800

    some fix;

--- fancyss_arm/config.json.js
+++ fancyss_arm/config.json.js
@@ -1,8 +1,8 @@
 {
-"build_date":"2021-09-04_17:57:06",
+"build_date":"2021-09-04_18:14:53",
 "description":"科学上网 for arm",
 "home_url":"Main_Ss_Content.asp",
-"md5":"fd3c24681368113859f791e5062d1db2",
+"md5":"8532456521218b008df4eaecaff6b5ed",
 "name":"planesocks",
 "tar_url": "https://raw.githubusercontent.com/fastbash/fancyss/master/fancyss_arm/planesocks.tar.gz",
 "title":"科学上网new",
