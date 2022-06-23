44e16fb68ba088b938b6780595f2a7525c3393bc
fastbash <yovfal@gmail.com>
2022-06-23 22:35:25 +0800

    repack;

--- packages/version.json.js
+++ packages/version.json.js
@@ -1,10 +1,10 @@
 {
   "name": "fancyss",
   "version": "3.0.4",
-  "md5_hnd_full": "e2d1f910c9d516cc47fc568fb91c0df5",
-  "md5_hnd_lite": "13868120197ae14d8636bc9b42b68204",
-  "md5_qca_full": "50b614597661048aa205c3948d95bf84",
-  "md5_qca_lite": "8b9684b91ad0f7ec74f6b7b55a1d9f19",
-  "md5_arm_full": "55a7af589673db3734f0afd261911f61",
-  "md5_arm_lite": "f5e54e85b0300fb65e4a2262f79243ba"
+  "md5_hnd_full": "4b6b2c1ece6a90dc07ec1d5d78b99790",
+  "md5_hnd_lite": "9a513a5d707018b702025f7ca94ea15a",
+  "md5_qca_full": "60b265ae40c106395fee949da645d905",
+  "md5_qca_lite": "1522658f3b48b3f7fecbf1d068692e95",
+  "md5_arm_full": "9cbe2245d1a33c03a7f75f1ef6a52039",
+  "md5_arm_lite": "da1576d19592a8367804cd52464b5fa6"
 }
