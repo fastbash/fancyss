a91a37a48862e1ca2566fd101fb19fff5fcea767
fastbash <yovfal@gmail.com>
2022-06-23 22:46:43 +0800

    repack use download filename in wget;

--- packages/version.json.js
+++ packages/version.json.js
@@ -1,10 +1,10 @@
 {
   "name": "fancyss",
   "version": "3.0.4",
-  "md5_hnd_full": "4b6b2c1ece6a90dc07ec1d5d78b99790",
-  "md5_hnd_lite": "9a513a5d707018b702025f7ca94ea15a",
-  "md5_qca_full": "60b265ae40c106395fee949da645d905",
-  "md5_qca_lite": "1522658f3b48b3f7fecbf1d068692e95",
-  "md5_arm_full": "9cbe2245d1a33c03a7f75f1ef6a52039",
-  "md5_arm_lite": "da1576d19592a8367804cd52464b5fa6"
+  "md5_hnd_full": "19477696e54952907fa5be0a66b91b2f",
+  "md5_hnd_lite": "7bbb12b279636a930ee49e3f5c1e7b50",
+  "md5_qca_full": "4c006f62d8522e2bad306a021d82d2ff",
+  "md5_qca_lite": "308455c1f33d775eea137719769065bb",
+  "md5_arm_full": "a70868413e0bc53031459451df0df9ef",
+  "md5_arm_lite": "4495dc17f421538b782a0f9b19110d48"
 }
