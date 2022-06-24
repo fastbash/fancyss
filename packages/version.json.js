c097e073d51dc1fa7f12465db1068aefa16a8f53
fastbash <yovfal@gmail.com>
2022-06-24 14:37:17 +0800

    fix rules.json;

--- packages/version.json.js
+++ packages/version.json.js
@@ -1,10 +1,10 @@
 {
   "name": "fancyss",
-  "version": "3.0.4.1",
-  "md5_hnd_full": "1a1d0bf9fd54ed8d6836a1e77b76def5",
-  "md5_hnd_lite": "aa678dda11c420085a80426014869cbc",
-  "md5_qca_full": "812361468b4f1f4f22202f2d4415f24b",
-  "md5_qca_lite": "f0ce5c9948c2e682170ac50906671ae7",
-  "md5_arm_full": "8c34733402b23689274340b357fe63de",
-  "md5_arm_lite": "3d3b4120641f355c3653d425fec962de"
+  "version": "3.0.4.2",
+  "md5_hnd_full": "8b3a6505203374334d2f939976c3ef56",
+  "md5_hnd_lite": "8a51a7c882fdd2db46402ada162acb5b",
+  "md5_qca_full": "4b9ca45221a922996ed581d1e7f25802",
+  "md5_qca_lite": "969138feef36c1f2a8dd811d324510a3",
+  "md5_arm_full": "f5bfd2667a18deb9bd238ecd5a75c474",
+  "md5_arm_lite": "ef32000f89327cf470ad86f6dfc4ac06"
 }
