ddc8087afc702493a7f44ae645dc5a600e70b097
fastbash <yovfal@gmail.com>
2022-08-22 12:12:06 +0800

    fix chnroute rule & count;

--- packages/version.json.js
+++ packages/version.json.js
@@ -1,10 +1,10 @@
 {
   "name": "fancyss",
-  "version": "3.0.4.3",
-  "md5_hnd_full": "e54120211d9f4ec212af840b7d202cee",
-  "md5_hnd_lite": "32438e154e39a5c7a51ebb695a294020",
-  "md5_qca_full": "c1235c09a4b98f722e0d47c9f81ae255",
-  "md5_qca_lite": "f7802d36035ba9a5df595bc00b078423",
-  "md5_arm_full": "d83fd9a76e8cfe9480d6e63309b2a2af",
-  "md5_arm_lite": "7a829df9be2c28c2d50838f76d4fe55a"
+  "version": "3.0.4.4",
+  "md5_hnd_full": "1f2c8180ece6f1b1fac22f931823ed16",
+  "md5_hnd_lite": "55b3ac410e68a4acc3c47463947aa379",
+  "md5_qca_full": "187576e5e69f82ea8a2ae2b3b4c9fe27",
+  "md5_qca_lite": "e23d43fd40ee4ade50e7374c1a471233",
+  "md5_arm_full": "4162bbbee3d69a1ed1d8d1d497f8f54a",
+  "md5_arm_lite": "78b3e45695c776c98dc84dd14ccbef9f"
 }
